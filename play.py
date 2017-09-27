26b1879c4733ff5c2a7fba8f5cd7715880354f32
Adam Kleczewski <adam.kleczewski@human.x.ai>
2017-09-26 23:55:57 -0400

    play

--- play.py
+++ play.py
@@ -2,14 +2,14 @@ import tensorflow as tf
 from agents.human_agent import HumanAgent
 from agents.random_agent import RandomAgent
 from agents.nn_agent import NeuralNetworkAgent
-from tic_tac_toe.tic_tac_toe import TicTacToe
+from tic_tac_toe import TicTacToe
 
 
 def main():
     with tf.Session() as sess:
         env = TicTacToe()
         nn_agent = NeuralNetworkAgent(sess, restore=True)
-        players = [nn_agent, nn_agent]
+        players = [HumanAgent(), nn_agent]
         # players = [RandomAgent(), RandomAgent()]
 
         env.play(players, verbose=True)
